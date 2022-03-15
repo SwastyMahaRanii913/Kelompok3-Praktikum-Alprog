4ed67b700458967c27d8eb93dcdbd616a4a81ab1
Swasty Maharani <94920692+Swastymaharani@users.noreply.github.com>
2022-03-15 23:00:14 +0800

    Update Soal no 6 .cpp

--- Modul 2/Soal no 6 .cpp
+++ Modul 2/Soal no 6 .cpp
@@ -4,6 +4,13 @@
 void awal();
 void angsuran();
 void keluar();
+void ulang();
+
+int main(){
+	awal();
+	angsuran();
+}
+
 
 float validasi(){
     float angka;
@@ -11,8 +18,8 @@ float validasi(){
     
     scanf("%f%c", &angka,&huruf);
     if(huruf != '\n'){
-        printf("---Masukkan salah!---\n");
-        printf(" Masukan ulang			: ");
+        printf("---Masukkan salah!---\n\n");
+        printf(" Masukan ulang dalam angka	: ");
         fflush(stdin);
         validasi();
     }
@@ -26,6 +33,14 @@ void angsuran(){
 	float pinjaman;
 	float bunga;
 	
+	int bulan;
+	int y;
+	float angsuran_pokok;
+	float angsuran_perbulan;
+	float total_bunga;
+	float total_angsuran;
+	float bungay;
+	
 	
 	printf(" Pokok Pinjaman			: ");
 	pinjaman=validasi();
@@ -33,6 +48,25 @@ void angsuran(){
 	bunga = validasi();
 	printf(" Lama Pinjaman (bulan)		: ");
 	lama_pinjaman=validasi();
+	
+	printf("\n\n\tBulan\t\tBunga\t\tAngsuran pokok\t\tAngsuran Perbulan\n");
+	for(y=1; y<=lama_pinjaman; y++){
+		
+		bulan=y;
+		angsuran_pokok = pinjaman/lama_pinjaman;
+		bungay = (pinjaman-((y-1)*angsuran_pokok))*bunga/1200;
+		angsuran_perbulan = bungay+angsuran_pokok;
+		
+		printf("\t %d\t\tRp. %.2f\tRp. %.2f\t\tRp. %.2f\n", bulan, bungay, angsuran_pokok, angsuran_perbulan);
+		
+		
+		total_bunga= total_bunga + bungay;
+		total_angsuran= total_angsuran + angsuran_perbulan;
+	}
+	printf("\n Total Bunga	:\t\t\t\t\t\t Rp. %.2f\n", total_bunga);
+	printf(" Total Angsuran	:\t\t\t\t\t\t Rp. %.2f", total_angsuran);
+	printf("\n\n\n----------------------------------------------------------------------------------\n");
+	ulang();
 }
 
 void keluar(){
@@ -50,7 +84,29 @@ void awal(){
 	printf("\n\n\n----------------------------------------------------------------------------------\n");
 }
 
-int main(){
-	awal();
-	angsuran();
+void ulang(){
+	int kembali;
+	
+	pilih:
+	printf("\n [1] Kembali Menghitung Angsuran");
+	printf("\n [2] Keluar Program");
+	printf("\n Pilihan Anda	[1 / 2]: ");
+	kembali=validasi();
+	
+	
+	switch(kembali){
+		case 1:
+			system ("cls");
+			awal();
+			angsuran();
+			break;
+		case 2:
+			system ("cls");
+			keluar();
+			break;
+		default:
+			printf("\n----Pilihan Tidak Sesuai!----\n");
+			goto pilih;
+			
+	}
 }
